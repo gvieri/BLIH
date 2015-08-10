889e64761ea879ab5ec35e1318c71150adbdcfc9
gvieri <g.vieri@gmail.com>
2015-08-10 20:34:58 +0200

    modified:   server/blih.create.tables.sql   more tables 	new file:   server/blih.initialize.data.sql initialize the tables

--- server/blih.create.tables.sql
+++ server/blih.create.tables.sql
@@ -7,6 +7,31 @@ alt decimal(10,7),
 message varchar (255), 
 PRIMARY KEY (`id`)
 
-) ENGINE=MYISAM CHARSET=utf8
+) ENGINE=MYISAM CHARSET=utf8 ;
+
+drop table if exists `users` ; 
+create table `users` (
+id int(11) NOT NULL AUTO_INCREMENT,
+username varchar(255) not null default '', 
+type int default 0, # type of user 0 for unidentified user. 
+crypto_type int  default 0, # 0 for ssl 1 for gpg
+public_key text not null default '' ,
+PRIMARY KEY (`id`)
+
+) ENGINE=MYISAM CHARSET=utf8 ;
+
+
+drop table if exists `crypto_mode`; 
+
+create table `crypto_mode` (
+type int(11) NOT NULL , #type of crypto mode
+name varchar(255) , 
+external boolean default FALSE, # if the crypto utility is a php lib or an external app 
+path varchar(255) , 
+INDEX ind(type),
+foreign key (type) 
+	references users(type),
+PRIMARY KEY(type)
+) ENGINE=MYISAM CHARSET=utf8 ;
 
 
---
+++ server/blih.initialize.data.sql
@@ -0,0 +1,5 @@
+insert into crypto_mode (type, name, external, path) values ('0', 'none', '0',''); 
+insert into crypto_mode (type, name, external, path) values ('1', 'openssl', '0',''); 
+
+
+insert into users (id,username,type,crypto_type,public_key) values ('0', 'default', '0', '0', ''); 
