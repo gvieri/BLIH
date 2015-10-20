4c3991ccb8be5edecbcd5947bbb5670ba5de1004
gvieri <g.vieri@gmail.com>
2015-10-20 18:34:12 +0200

    used mysqli instead of mysql_ ...

--- admin/dump_table_box_csv.php
+++ admin/dump_table_box_csv.php
@@ -10,8 +10,11 @@
 
 
 	require_once("./db_conf.php");
+	$file="dbcontents"; 
+ 
 	    //Create Database connection
 	$db = mysqli_connect($dbhost,$dbuser,$dbpwd,$dbname);
+
 	if (!$db) {
 	        die('Could not connect to db: ' . mysqli_error());
 	}
@@ -23,21 +26,23 @@
 	$query="select * from geo_data where lat is not null and lat>'$lat0' and lat<'$lat1' and lng>'$lng0' and lng<'$lng1' ";
 //		echo "!$query|";
 	$result = mysqli_query($db, $query); 
-	     
 	    //Create an array
-	$json_response = array();
-	     
+	$csv_output = ""; 
 	while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)) {
-	        $row_array['message'] = $row['message'];
-	        $row_array['lat'] = $row['lat'];
-	        $row_array['lng'] = $row['lng'];
+	        $csv_output .= $row['lat'].",";
+	        $csv_output .= $row['lng'].",";
+	        $csv_output .= $row['message']."\n";
 	         
 	        //push the values in the array
-	        array_push($json_response,$row_array);
 	}
-	echo json_encode($json_response);
 	     
 	    //Close the database connection
 	mysqli_close($db);
-	  
+	$filename = $file."_".date("Y-m-d_H-i",time());
+	header("Content-type: application/vnd.ms-excel; ");	
+	header("Content-disposition: attachment;" );
+	header( "Content-disposition: filename=".$filename.".csv");
+	echo "\xEF\xBB\xBF"; //UTF-8 BOM
+	echo $csv_output;
+	exit;	  
 ?>
