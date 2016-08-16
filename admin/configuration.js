f7d57e80fce6ed6279575a0bd4a381f39d17ff19
gvieri <g.vieri@gmail.com>
2016-08-16 16:16:34 +0200

    initial commit of configuration file

---
+++ admin/configuration.js
@@ -0,0 +1,40 @@
+/***
+
+BLIH (c) Giovambattista Vieri 2015,2016
+
+All rights reserved 
+
+https://github.com/gvieri/BLIH/
+
+configuration.js 
+
+This file contains configuration data. 
+
+License: see github pages
+
+*/
+
+
+var tileLayerConfVal		= 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png' ;  //// tiles server 
+
+var attributionCopyConfVal	= '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, '; // (C) delle tiles 
+
+var attributionLicenseConfVal	= '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ';  // license delle tiles 
+
+var attributionThirdVal		= '<a href="/privacydir/longinfo.html">privacy policy</a>'; 
+
+
+var maxZoomConfVal		= 18 ;  // max zoom allowable 
+
+var splashScreenTimeOutConfVal	= 6000; 
+
+var initLatConfVal		= 41.954;	/**** Rome Italy  ****/
+var initLngConfVal		= 12.547;	/****   Area      ****/
+
+var consoleLoggingFlag		= false ; 
+
+/***********
+
+
+
+***********/
